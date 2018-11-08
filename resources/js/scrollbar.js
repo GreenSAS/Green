c292301fb702ea33f28f552c5ff18d0110fbe071
Green <greenweb.sas@gmail.com>
2018-11-08 15:12:48 +0100

    Final version

---
+++ resources/js/scrollbar.js
@@ -0,0 +1,14 @@
+// When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("header").style.background = "#403e2c";
+    document.getElementById("logo").style.display = "block";
+    document.getElementById("logo-white").style.display = "none";
+  } else {
+    document.getElementById("header").style.background = "transparent";
+    document.getElementById("logo").style.display = "none";
+    document.getElementById("logo-white").style.display = "block";
+  }
+}
