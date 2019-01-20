dcd84b4a7b4187cbeb853a1b4fa78d736767646c
Green <greenweb.sas@gmail.com>
2019-01-20 15:03:26 +0100

    DES CHANGEMENTS EN MASSE LOL

--- resources/js/menuslide.js
+++ resources/js/menuslide.js
@@ -1,12 +1,14 @@
 var click = document.querySelectorAll('.menuslide');
 var menu = document.querySelector('.st-container');
 var pusher = document.querySelector('.pusher');
+var pusher2 = document.querySelector('.video-pusher');
 
 for (var i = 0; i < click.length; i++) {
   click[i].addEventListener('click', addClass)
 }
 
 pusher.addEventListener('click', closeMenu);
+pusher2.addEventListener('click', closeMenu2);
 
 function addClass(e) {
 
@@ -18,6 +20,15 @@ function addClass(e) {
 function closeMenu(el) {
 
   if (el.target.classList.contains('pusher')) {
+    menu.classList.remove('effect');
+    menu.classList.remove('menu-open');
+
+  }
+}
+
+function closeMenu2(el) {
+
+  if (el.target.classList.contains('.video-pusher')) {
     menu.classList.remove('effect');
     menu.classList.remove('menu-open');
 
